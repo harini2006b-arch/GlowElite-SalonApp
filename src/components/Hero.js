7727355490d6e485b90b74e2f8267c1c278e4467
Harini B <harini2006b@gmail.com>
2026-06-21 17:48:30 +0530

    GlowElite-SalonApp

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,15 @@
 import "../styles/Hero.css";
 import React, { useState, useEffect } from "react";
-
-function Hero() {
-
-  const images = [
+ const images = [
     "https://images.unsplash.com/photo-1521590832167-7bcbfaa6381f?auto=format&fit=crop&w=1200&q=80",
     "https://images.unsplash.com/photo-1621605815971-fbc98d665033?auto=format&fit=crop&w=1200&q=80",
     "https://images.unsplash.com/photo-1560066984-138dadb4c035?auto=format&fit=crop&w=1200&q=80"
   ];
+
+
+function Hero() {
+
+
 
   const [currentImage, setCurrentImage] = useState(0);
 
@@ -17,7 +19,7 @@ function Hero() {
     }, 4000);
 
     return () => clearInterval(interval);
-  }, [images.length]);
+  }, []);
 
 
 
